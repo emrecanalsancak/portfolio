27bf2ceef249f97b5130e24e88ba813973b1339b
emrecanalsancak <88661179+emrecanalsancak@users.noreply.github.com>
2023-11-04 21:30:06 +0300

    Update github username

--- data.js
+++ data.js
@@ -13,7 +13,7 @@ const data = [
     name: "Rest Countries Api",
     img: REST,
     demoLink: "https://countries-rest-api-v3.netlify.app",
-    gitLink: "https://github.com/ealsa/rest-countries",
+    gitLink: "https://github.com/emrecanalsancak/rest-countries",
   },
   {
     id: 2,
@@ -21,15 +21,15 @@ const data = [
     name: "Weather App",
     img: reactWeather,
     demoLink: "https://react-forecast.netlify.app/",
-    gitLink: "https://github.com/ealsa/react-openmeteo-forecast",
+    gitLink: "https://github.com/emrecanalsancak/react-openmeteo-forecast",
   },
   {
     id: 3,
     title: "JavaScript",
     name: "Education Website",
     img: education,
-    demoLink: "https://ealsa.github.io/Education-Website/",
-    gitLink: "https://github.com/ealsa/Education-Website",
+    demoLink: "https://emrecanalsancak.github.io/Education-Website/",
+    gitLink: "https://github.com/emrecanalsancak/Education-Website",
   },
   {
     id: 4,
@@ -37,7 +37,7 @@ const data = [
     name: "E-Commerce",
     img: ecommerce,
     demoLink: "https://basic-react-ecommerce.netlify.app/",
-    gitLink: "https://github.com/ealsa/react-e-commerce",
+    gitLink: "https://github.com/emrecanalsancak/react-e-commerce",
   },
 
   {
@@ -45,8 +45,8 @@ const data = [
     title: "JavaScript",
     name: "Math Sprint",
     img: math,
-    demoLink: "https://ealsa.github.io/Math-Sprint/",
-    gitLink: "https://github.com/ealsa/Math-Sprint",
+    demoLink: "https://emrecanalsancak.github.io/Math-Sprint/",
+    gitLink: "https://github.com/emrecanalsancak/Math-Sprint",
   },
   {
     id: 6,
@@ -54,7 +54,7 @@ const data = [
     name: "Portfolio Website",
     img: portfolio,
     demoLink: "https://ec-alsancak.netlify.app/",
-    gitLink: "https://github.com/ealsa/Portfolio-project",
+    gitLink: "https://github.com/emrecanalsancak/Portfolio-project",
   },
   {
     id: 7,
